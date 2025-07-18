f66047b6f34b557bfacbf5933ff2642a0fe28620
Lucasgrn <lucasgabriel.ogn@gmail.com>
2025-07-18 16:35:12 -0300

    feat: Add image host

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images:{
+    remotePatterns:[new URL('http://i.annihil.us/u/prod/marvel/i/mg/**')]
+  }
 };
 
 export default nextConfig;
