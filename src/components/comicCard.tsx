d3dc4b014d61c20d4ef11c3565ef3cb878f2d0e9
Lucasgrn <lucasgabriel.ogn@gmail.com>
2025-07-19 20:50:04 -0300

    feat: Create comics card

---
+++ src/components/comicCard.tsx
@@ -0,0 +1,15 @@
+import { Comic } from "@/types/comic";
+import Image from "next/image";
+
+export default function ComicCard(props: Comic) {
+
+  return (
+    <div className="w-auto h-110 border-1 border-black rounded-lg bg-[#D9D9D9] m-5 ">
+      <Image className="rounded-md w-full h-1/2" src={`${props.thumbnail?.path}.${props.thumbnail?.extension}`} width={200} height={200} alt={`${props.title} image`} />
+      <div className="m-5">
+        <h1 className="text-xl font-bold">{props.title}</h1>
+        <p>{props.description.length < 1 ? props.description.slice(0, 90) + "..." : "Empty description"}</p>
+      </div>
+    </div>
+  )
+}
