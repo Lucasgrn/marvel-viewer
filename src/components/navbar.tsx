af7a4fff1740bb26db6bacb30ebc2046a0bb1908
Lucasgrn <lucasgabriel.ogn@gmail.com>
2025-07-19 13:13:48 -0300

    feat: Add icon in history

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,5 @@
 "use client"
+import Image from "next/image"
 import Link from "next/link"
 import { redirect } from "next/navigation"
 import { useState } from "react"
@@ -22,7 +23,13 @@ export default function Navbar() {
           onChange={(event) => { setInput(event.target.value) }}
           onKeyDown={handleKey}
         />
-        <h1>History</h1>
+        <Link href={'/history'} className="flex justify-between self-center  w-auto h-auto">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 mr-1">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+          </svg>
+
+          <h1>History</h1>
+        </Link>
       </div>
     </nav>
   )
