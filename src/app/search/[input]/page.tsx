8fd8f3b64a407716e1061f28d599dfe7efeaeee0
Lucasgrn <lucasgabriel.ogn@gmail.com>
2025-07-19 20:51:14 -0300

    feat: Add pagination to search

--- src/app/search/[input]/page.tsx
+++ src/app/search/[input]/page.tsx
@@ -1,15 +1,21 @@
 import Card from "@/components/card"
+import Paginator from "@/components/pagination"
 import { api } from "@/service/api"
 import { Character } from "@/types/character"
 
-export default async function Search({ params }: {
-  params: Promise<{ input: string }>
+export default async function Search({ params, searchParams }: {
+  params: Promise<{ input: string }>,
+  searchParams: Promise<{ page: string }>
 }) {
   const search = await params
-  const response = await api('/characters', `nameStartsWith=${search.input}`)
+  const query = await searchParams
+  const queryPage = parseInt(query.page, 10)
+  const limit = 20
+  const response = await api('/characters', `nameStartsWith=${search.input}&offset=${(queryPage - 1) * limit}`)
   const characters = response.data
+  const page = Math.ceil((response.offset + 1) / limit)
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col justify-between min-h-screen">
       <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-2 w-3/4 self-center">
         {characters.map((item: Character) => {
           return (
@@ -17,6 +23,7 @@ export default async function Search({ params }: {
           )
         })}
       </div>
+      <Paginator offset={response.offset + 1} total={response.total} count={limit} page={page} />
     </div>
   )
 }
